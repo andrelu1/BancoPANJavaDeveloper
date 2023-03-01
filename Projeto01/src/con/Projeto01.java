c7fd50157c359a1e863e2d2c181a65f8a5f5ad11
andrelu1 <andre.mastrocola@fatec.sp.gov.br>
2023-03-01 14:31:07 -0300

    Add files via upload

---
+++ Projeto01/src/con/Projeto01.java
@@ -0,0 +1,20 @@
+package con;
+
+import con.model.Gato;
+
+public class Projeto01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//int a = 1;
+		//int b = 2;
+        //System.out.println("Hello World"+(a+b));
+        Gato gato = new Gato();
+        Livros livros = new Livros(); 
+	}
+
+}
+class Livros {
+	private String nome;
+	private String npag;
+}
---
+++ Projeto01/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author andre
+ *
+ */
+module Projeto01 {
+}
